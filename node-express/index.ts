f6127f08f056120d1d56acca3fd31faf8098b380
Roges <rogeslucas@gmail.com>
2021-11-29 23:04:09 -0300

    express simple rest example

--- node-express/index.ts
+++ node-express/index.ts
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser'
 import express from 'express'
 import { createServer } from 'http'
 import morgan from 'morgan'
@@ -7,6 +8,47 @@ const PORT = 3000
 
 const app = express()
 app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.all('/dishes', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    next()
+})
+
+app.get('/dishes', (req: express.Request, res: express.Response) => {
+    res.end('Sending all the dishes to you!')
+})
+
+app.post('/dishes', (req: express.Request, res: express.Response) => {
+    res.end(`Adding the dish ${req.body.name}`)
+})
+
+app.put('/dishes', (req: express.Request, res: express.Response) => {
+    res.statusCode = 403
+    res.end('PUT operation not supported on /dishes')
+})
+
+app.delete('/dishes', (req: express.Request, res: express.Response) => {
+    res.end('Deleting all the dishes!')
+})
+
+app.get('/dishes/:dishId', (req: express.Request, res: express.Response) => {
+    res.end(`Sending dish ${req.params.dishId} to you!`)
+})
+
+app.post('/dishes/:dishId', (req: express.Request, res: express.Response) => {
+    res.statusCode = 403
+    res.end(`POST operation not supported on /dishes/${req.params.dishId}`)
+})
+
+app.put('/dishes/:dishId', (req: express.Request, res: express.Response) => {
+    res.end(`Updating dish ${req.params.dishId}`)
+})
+
+app.delete('/dishes/:dishId', (req: express.Request, res: express.Response) => {
+    res.end(`Deleting dish ${req.params.dishId}`)
+})
 
 app.use(express.static(`${__dirname}/public`))
 
