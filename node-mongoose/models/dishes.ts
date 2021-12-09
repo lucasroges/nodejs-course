553f1d1971d782fed331133912125f9f20c30041
Lucas Roges <lucasrogesaraujo@gmail.com>
2021-12-09 16:29:15 -0300

    update mongoose dishes model

--- node-mongoose/models/dishes.ts
+++ node-mongoose/models/dishes.ts
@@ -1,5 +1,22 @@
 import { model, Schema } from 'mongoose'
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: false
+    },
+    author: {
+        type: String,
+        required: true
+    }
+})
+
 const dishSchema = new Schema({
     name: {
         type: String,
@@ -9,7 +26,8 @@ const dishSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 })
